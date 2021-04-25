17cbdabae3b37e53176be035220327abaff1f76f
Felicia Heiney <bhfelicia@gmail.com>
2021-04-25 16:37:24 -0400

    sets up fs for front end and webpack

---
+++ client/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Is this working?</h1>
+        <Router></Router>
+      </div>
+    );
+  }
+}
+
+export default App;
