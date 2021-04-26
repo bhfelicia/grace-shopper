3da3db03d056d0fef6aa6f3e66c4da12cceceeea
Linda Nzeukang <lindalauranzeukang@gmail.com>
2021-04-26 10:32:17 -0500

    model-order

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 const { db } = require("./db");
 const User = require("./models/User");
 const Product = require("./models/Product");
+const Order = require("./models/Order");
 //const seed = require('./seed');
 
 const init = async () => {
@@ -14,6 +15,10 @@ const init = async () => {
 };
 
 //state your model associations (hasOne etc)
+User.hasMany(Order);
+Order.belongsTo(User);
+Order.hasMany(Product);
+Product.belongsToMany(Order, {through: "order_product"});
 
 //export your db and Models (so they all can be imported from a single central location)
 
