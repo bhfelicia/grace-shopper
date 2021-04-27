fcc2f56dc32d7f18b77fd8498d481606d7a91841
Inderprit Singh <singhinderprit1992@gmail.com>
2021-04-26 21:21:11 -0400

    fixed travis spec files

--- test/spec.js
+++ test/spec.js
@@ -1,19 +1,23 @@
 const { expect } = require("chai");
 const Product = require("../server/db/models/Product");
-const { init } = require("../server/db/index");
+const initTest = require("./testDb");
 
 describe("addition", () => {
   it("adds 3 and 4", () => {
     expect(3 + 4).to.equal(7);
   });
 });
-var products;
-describe("Products", async () => {
+var seedData;
+describe("Models", async () => {
   beforeEach(async () => {
-    products = await init();
+    seedData = await initTest();
   });
 
   it("adds products successfully", () => {
-    expect(products.length).to.equal(3);
+    expect(seedData.productArr.length).to.equal(3);
+  });
+
+  it("adds users successfully", () => {
+    expect(seedData.usersArr.length).to.equal(4);
   });
 });
