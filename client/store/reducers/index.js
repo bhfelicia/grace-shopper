74efce5ceb14c1b0e6058cff537e2826d065cfaf
Felicia Heiney <bhfelicia@gmail.com>
2021-04-28 11:05:49 -0400

    adds order subreducer to deal with created orders (order history) and current cart (order in progress)

--- client/store/reducers/index.js
+++ client/store/reducers/index.js
@@ -2,7 +2,12 @@ import { combineReducers } from "redux";
 
 import categoryReducer from "./categoryReducer";
 import productReducer from "./productReducer";
+import orderReducer from "./orderReducer";
 
-const rootReducer = combineReducers({ categoryReducer, productReducer });
+const rootReducer = combineReducers({
+  categoryReducer,
+  productReducer,
+  orderReducer,
+});
 
 export default rootReducer;
