4a161cf72b5a484be0537168bc6ac4e92d13d205
Arjan Mitra <arjanmitra@hotmail.com>
2021-05-07 18:14:26 -0400

    more changes

--- client/components/NAVBAR/Home.js
+++ client/components/NAVBAR/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 // import { connect } from "react-redux";
 // import { Link } from "react-router-dom";
 // import AllProducts from "../PRODUCTS/AllProducts";
@@ -9,7 +9,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div id="welcome" style={{ marginBottom: "2rem" }}>
+    <div id="welcome" style={{ marginTop: '2rem' }}>
       <h1>welcome to dynamic ceramics!</h1>
       <h3>
         we source our unique, hand-crafted pieces from talented artisans
